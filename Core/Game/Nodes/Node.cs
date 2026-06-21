5e6e30562e847f006ae0b23b7d9be8255d47157e
TheZQS <thezqs4@gmail.com>
2026-06-21 18:40:04 -0400

    Eliminando resolucion de los charts

--- Core/Game/Nodes/Node.cs
+++ Core/Game/Nodes/Node.cs
@@ -1,9 +1,19 @@
-using osu.Framework.Graphics;
-using osu.Framework.Graphics.Containers;
-
 namespace Core.Game.Nodes;
 
-public partial class Node : Container
+public partial class Node : CompositeDrawable
 {
+    protected override void Update()
+    {
+        Vector2 gameSize = Gameplay.GameSize;
 
+        if (gameSize == Vector2.Zero) return;
+
+        // Primera prueba
+        Position = gameSize / 2;
+    }
+
+    public void AddChild(Node node)
+    {
+        AddInternal(node);
+    }
 }
--- Data/Game/Chart.cs
+++ Data/Game/Chart.cs
@@ -5,11 +5,5 @@ namespace Data.Game;
 /// <summary> La clase donde se guardan todos los datos de un Chart </summary>
 public class Chart
 {
-    /// <summary> 
-    /// La resolucion del mapa. 
-    /// Puede ser ignorada si GameSettings.ForceResolution es true
-    /// </summary>
-    public Vector2 Resolution { get; set; } = new();
-    
     public List<ObjectData> Objects { get; set; } = [];
 }
