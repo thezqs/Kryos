205757e71192f59219a2a96e8ac5f67d66f550ba
TheZQS <thezqs4@gmail.com>
2026-06-21 18:39:53 -0400

    Eliminando resolucion

--- Data/Settings/GameSettings.cs
+++ Data/Settings/GameSettings.cs
@@ -5,13 +5,5 @@ namespace Data.Settings;
 /// <summary> La clase encargada de guardar las configuraciones el juego. </summary>
 public class GameSettings
 {
-    /// <summary> Fuerza la resolucion a Resolution </summary>
-    public bool ForceResolution = false;
-
-    /// <summary>
-    /// La resolucion de todos los mapas.
-    /// Solo funciona si ForceResolution True.
-    /// </summary>
-    public Vector2 Resolution = new(1366, 768);
 
 }
